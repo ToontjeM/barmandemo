1e6eb26ec9039c2a3fbc880bc992a3fe6d1790cb
Ton Machielsen--replace-all <ton@machielsen.net>
2025-03-07 10:17:49 +0100

    README.md and cosmetig in 12

--- 12-barmanhost_recover_db.sh
+++ 12-barmanhost_recover_db.sh
@@ -6,14 +6,18 @@ red=$(tput setaf 1)
 green=$(tput setaf 2)
 normal=$(tput sgr0)
 
-printf "${green}Shutdown Database${normal}\n\n" 
+printf "${green}Shutdown Database${normal}\n" 
+printf "${red}/usr/pgsql-17/bin/pg_ctl --pgdata=/var/lib/pgsql/17/data stop${normal}\n\n" 
 ssh postgres@pghost /usr/pgsql-17/bin/pg_ctl --pgdata=/var/lib/pgsql/17/data stop
 
-printf "${green}Backup and remove broken database${normal}\n\n" 
+printf "${green}\nBackup and remove broken database${normal}\n" 
+printf "${red}cp -a /var/lib/pgsql/17/data /var/lib/pgsql/17/old_data && rm -rf /var/lib/pgsql/17/data/*${normal}\n\n" 
 ssh postgres@pghost "cp -a /var/lib/pgsql/17/data /var/lib/pgsql/17/old_data && rm -rf /var/lib/pgsql/17/data/*"
 
-printf "${green}Recover last backup${normal}\n\n" 
+printf "${green}\nRecover last backup${normal}\n" 
+printf "${red}barman recover --remote-ssh-command 'ssh postgres@pghost' pghost latest /var/lib/pgsql/17/data${normal}\n\n" 
 barman recover --remote-ssh-command 'ssh postgres@pghost' pghost latest /var/lib/pgsql/17/data
 
-printf "${green}Start database${normal}\n\n" 
+printf "${green}Start database${normal}\n" 
+printf "${red}/usr/pgsql-17/bin/pg_ctl --pgdata=/var/lib/pgsql/17/data -l /var/lib/pgsql/17/data/log/pg.log start${normal}\n\n" 
 ssh postgres@pghost "/usr/pgsql-17/bin/pg_ctl --pgdata=/var/lib/pgsql/17/data -l /var/lib/pgsql/17/data/log/pg.log start"
